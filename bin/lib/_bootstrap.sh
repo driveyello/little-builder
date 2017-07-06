59f0ad6e5a0c1d831784b37d88cac7553a052ab1
Peter Vahaviolos <peter@taledo.com.au>
2017-07-06 10:26:10 +1000

    remove sudo

--- bin/lib/_bootstrap.sh
+++ bin/lib/_bootstrap.sh
@@ -4,9 +4,9 @@ function ensure_package() {
     local package_name=$1
     echo "Ensuring package $package_name is installed..."
     if which yum ; then
-        sudo yum install --assumeyes $package_name
+        yum install --assumeyes $package_name
     elif which apt-get ; then
-        sudo apt-get install -y $package_name
+        apt-get install -y $package_name
     elif  which brew ; then
         brew install $package_name
     else
